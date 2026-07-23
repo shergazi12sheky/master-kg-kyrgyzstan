be3c5912b2122cc2e332e079e269cc4fd1753338
shergazi12sheky <shergazi12@gmail.com>
2026-07-23 21:21:32 +0600

    Add product search function
    Signed-off-by: shergazi12sheky <shergazi12@gmail.com>

--- script.js
+++ script.js
@@ -1,32 +1,97 @@
-// ILBIRS MARKETPLACE
-
-// Издөө функциясы
-
-function searchProduct() {
-    let text = document.querySelector("input").value;
-
-    if(text === "") {
-        alert("Введите название товара или услуги");
-    } else {
-        alert("Поиск: " + text);
-    }
-}
+// ILBIRS MARKETPLACE JAVASCRIPT
 
 
 // Корзина
 
 let cart = 0;
 
+
 let buttons = document.querySelectorAll(".product button");
+
 
 buttons.forEach(function(button){
 
-    button.onclick = function(){
+button.onclick = function(){
 
-        cart++;
+cart++;
 
-        alert("Товар добавлен в корзину. Товаров: " + cart);
+alert("Товар добавлен в корзину: " + cart);
 
-    }
+}
 
 });
+
+
+
+
+// Поиск товаров
+
+function searchProducts(){
+
+let input = document
+.getElementById("searchInput")
+.value
+.toLowerCase();
+
+
+let products = document
+.querySelectorAll(".product");
+
+
+products.forEach(function(product){
+
+
+let text = product
+.innerText
+.toLowerCase();
+
+
+if(text.includes(input)){
+
+product.style.display = "block";
+
+}else{
+
+product.style.display = "none";
+
+}
+
+
+});
+
+}// ILBIRS Marketplace Search
+
+function searchProducts(){
+
+let input = document
+.getElementById("searchInput")
+.value
+.toLowerCase();
+
+
+let products = document
+.querySelectorAll(".product");
+
+
+products.forEach(function(product){
+
+
+let name = product
+.innerText
+.toLowerCase();
+
+
+if(name.includes(input)){
+
+product.style.display = "block";
+
+}else{
+
+product.style.display = "none";
+
+}
+
+
+});
+
+}
