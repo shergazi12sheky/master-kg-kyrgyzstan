418e2a5fbe36697769befff4e4f34889073e1019
shergazi12sheky <shergazi12@gmail.com>
2026-07-23 21:24:37 +0600

    Add cart function
    Signed-off-by: shergazi12sheky <shergazi12@gmail.com>

--- script.js
+++ script.js
@@ -20,7 +20,23 @@ alert("Товар добавлен в корзину: " + cart);
 }
 
 });
+// ILBIRS Cart
 
+let cart = 0;
+
+let buttons = document.querySelectorAll(".product button");
+
+buttons.forEach(function(button){
+
+button.onclick = function(){
+
+cart++;
+
+alert("Товар добавлен в корзину: " + cart);
+
+}
+
+});
 
 
 
